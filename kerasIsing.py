daebef8af11800b5c9dfb260fbec081a6109db9e
nihilsciens <33319318+nihilsciens@users.noreply.github.com>
2018-02-14 14:28:59 +0100

    Made sure network evaluates test data separately

--- kerasIsing.py
+++ kerasIsing.py
@@ -10,8 +10,9 @@ import numpy
 ################
 # fix random seed for reproducibility
 numpy.random.seed(7)
-# load data set
+# load data set and test set
 dataset = numpy.loadtxt("IsingValues.txt", delimiter=",")
+testset = numpy.loadtxt("IsingTest.txt", delimiter=",")
 # note shape of indata
 size_y = dataset.shape[0]
 size_x = dataset.shape[1]
@@ -19,15 +20,18 @@ border = size_x - 2
 # split into input (X) and output (Y) variables
 X = dataset[:,0:border]
 Y = dataset[:,border:size_x]
+# do the same for test data
+Xt = testset[:,0:border]
+Yt = testset[:,border:size_x]
 
 #########
 # MODEL #
 #########
 # Define model
 model = Sequential()
-model.add(Dense(22, input_dim=100, activation='relu'))
+model.add(Dense(border, input_dim=border, activation='relu'))
 model.add(Dense(8, activation='sigmoid'))
-model.add(Dense(2, activation='sigmoid')) # Dense(1, ....)
+model.add(Dense(2, activation='sigmoid'))
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
@@ -35,18 +39,18 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 # BACKPROPAGATION #
 ###################
 # Fit the model
-model.fit(X, Y, epochs=150, batch_size=10)
+model.fit(X, Y, epochs=1500, batch_size=10)
 
 ##############
 # EVALUATION #
 ##############
-scores = model.evaluate(X, Y)
+scores = model.evaluate(Xt, Yt)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 ###########
 # PREDICT #
 ###########
 # calculate predictions
-predictions = model.predict(X)
+predictions = model.predict(Xt)
 # print predictions
 print(predictions)
